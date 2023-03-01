c3c4146656511af33de2081a1e4d86637d5137ab
Gryphire <nastasia.griffioen@gmail.com>
2023-03-01 16:25:38 +0100

    Implemented data choice flexibility
    Previous versions required some Facebook Messenger data in order to work. Now data selection is more flexible and users can choose whether or not they want to include Facebook Messenger data, ánd whether or not they want to include Twitter data. Script should now thus work with 1) only FB Messenger data, 2) only Twitter data, 3) both, and 4) none.

--- 1JSONparseToOutput.py
+++ 1JSONparseToOutput.py
@@ -1,3 +1,7 @@
+
+#-----------------------------------------#
+## IMPORTING THE NECESSARY LIBRARIES
+
 import json
 import re
 import traceback
@@ -13,13 +17,19 @@ from difflib import get_close_matches
 # Prior to running, make sure snscrape is installed via VSCode terminal using pip3 install snscrape
 import snscrape.modules.twitter as snt
 
+#-----------------------------------------#
 ## GENERAL VARIABLE SETUP
 # Select files in your current directory that begin with 'data_'
 dataFiles = glob('data_*')
 
-# Check which files have been selected
-#print('The following data files have been found: '+ str(dataFiles))
+#-----------------------------------------#
+## PROCESS FOR DATA THAT NEEDS TO BE RETRIEVED FROM LOCAL DATA LOG FILES
 
+# DATA INCLUSION CHECK
+# Ask user if they want to use Facebook Messenger data files or now (if not, skip to next data type)
+FBMesDecision = input("Would you like to scrape data from your local Facebook Messenger data files? Y/N ")
+
+# INITIALIZE COUNTERS
 # Set a counter for Facebook Messenger file looping (will be used later to make sure name check is done only once, for first file)
 fbmescounter = 0
 
@@ -33,74 +43,86 @@ for each_file in dataFiles:
   # Pring to the console which data file is being processed now
   #print(each_file)
 
-  ## FACEBOOK MESSENGER DATA
-  with open(each_file, encoding='utf-8') as f:
-    textdata = json.load(f)
-
-  if each_file.find("fb_mes") != -1:
-
-    # Update file counter (because we want to run a name check later, but only once)
-    fbmescounter += 1
+  if (FBMesDecision == 'Y') or (FBMesDecision == 'y'):
     
-    try:
-      # Extract message content from data file
-      messages = map_except(lambda message: message['content'], textdata['messages'],KeyError)
-    except Exception as e:
-      #traceback.print_exc()
-      print('Working on it...')
+    #-----------------------------------------# 
+    
+    ## FACEBOOK MESSENGER DATA
+    
+    with open(each_file, encoding='utf-8') as f:
+      textdata = json.load(f)
 
-    try:  
-      # this replaces the offending string with an actual utf-8 curly single quote!
-      # https://patrickwu.space/2017/11/09/python-encoding-issue/
-      for x in messages:
-        output_string = x
-        output_string = re.sub(r'[\xc2-\xf4][\x80-\xbf]+',lambda m: m.group(0).decode('unicode-escape').encode('latin1').decode('utf8'),x) 
-        newdata.append(output_string)
-    except Exception as e:
-      #traceback.print_exc()
-      print('Still working on it...')
+    if each_file.find("fb_mes") != -1:
 
-    # new senders variable
-    senders = map(lambda sender: sender['sender_name'], textdata['messages'])
-    # fix ASCII-interpretation of what is actually UTF-8 coded text
-    senders = [y.encode('utf-8').decode('utf-8') for y in senders]
+      # Update file counter (because we want to run a name check later, but only once)
+      fbmescounter += 1
+      print('Currently working on Facebook Messenger data file number ' + str(fbmescounter))
+      
+      try:
+        # Extract message content from data file
+        messages = map_except(lambda message: message['content'], textdata['messages'],KeyError)
+      except Exception as e:
+        #traceback.print_exc()
+        print('Working on it...')
 
-    if fbmescounter == 1:
-      # For the very first Facebook Messenger file, we want to check which name in the senders file is the user's, 
-      # so we know what name to use in the future
-      # So first we need to check the different values present in the list, 
-      # we do this by turning the list into a set
-      setOfSenders = set(senders)
-      # Next we turn the set back into a list so we can access the items mentioned within (this is not possible with a set)
-      listOfSenders = list(setOfSenders)
-      # Present these options to the user so they can select their own name
-      usersNameIndex = int(input('The following participants have been detected in your first Facebook Messenger file: '+ str(listOfSenders) +'. Please tell us which of these is you by typing in the number associated with the name position in the list (e.g., 1 if the first name is yours): '))
-      # Now we can save the user's  name from the list of senders. 
-      # Since Python is zero-indexed, we need to correct the input number to 
-      # match the positions as Python counts them (i.e., first position is indexed as 0)
-      # Note: it may very well be that this is not close to the person's actual name since this is based on the
-      # Facebook username. If you have NassieBal as your username on Facebook, then this is what will be used as the 
-      # userIdentifier. The actual name used is not of great importance, it is simply important to make sure that 
-      userIdentifier = listOfSenders[usersNameIndex-1]
+      try:  
+        # this replaces the offending string with an actual utf-8 curly single quote!
+        # https://patrickwu.space/2017/11/09/python-encoding-issue/
+        for x in messages:
+          output_string = x
+          output_string = re.sub(r'[\xc2-\xf4][\x80-\xbf]+',lambda m: m.group(0).decode('unicode-escape').encode('latin1').decode('utf8'),x) 
+          newdata.append(output_string)
+      except Exception as e:
+        #traceback.print_exc()
+        print('Still working on it...')
 
-    # Printing to text file (hopefully), each file's data to a new line
-    with open("output_messages.txt", "a", encoding='utf-8') as f:
-      print(list(newdata), file=f)
+      # new senders variable
+      senders = map(lambda sender: sender['sender_name'], textdata['messages'])
+      # fix ASCII-interpretation of what is actually UTF-8 coded text
+      senders = [y.encode('utf-8').decode('utf-8') for y in senders]
 
-    with open("output_senders.txt", "a") as f:
-      print(list(senders), file=f)
+      if fbmescounter == 1:
+        # For the very first Facebook Messenger file, we want to check which name in the senders file is the user's, 
+        # so we know what name to use in the future
+        # So first we need to check the different values present in the list, 
+        # we do this by turning the list into a set
+        setOfSenders = set(senders)
+        # Next we turn the set back into a list so we can access the items mentioned within (this is not possible with a set)
+        listOfSenders = list(setOfSenders)
+        # Present these options to the user so they can select their own name
+        usersNameIndex = int(input('The following participants have been detected in your first Facebook Messenger file: '+ str(listOfSenders) +'. Please tell us which of these is you by typing in the number associated with the name position in the list (e.g., 1 if the first name is yours): '))
+        # Now we can save the user's  name from the list of senders. 
+        # Since Python is zero-indexed, we need to correct the input number to 
+        # match the positions as Python counts them (i.e., first position is indexed as 0)
+        # Note: it may very well be that this is not close to the person's actual name since this is based on the
+        # Facebook username. If you have NassieBal as your username on Facebook, then this is what will be used as the 
+        # userIdentifier. The actual name used is not of great importance, it is simply important to make sure that 
+        userIdentifier = listOfSenders[usersNameIndex-1]
+
+      # Printing to text file (hopefully), each file's data to a new line
+      with open("output_messages.txt", "a", encoding='utf-8') as f:
+        print(list(newdata), file=f)
+
+      with open("output_senders.txt", "a") as f:
+        print(list(senders), file=f)
 
 # Let the user know where we are in the process
-print('Facebook Messenger data scraped. Moving on to Twitter now.')
+if FBMesDecision == 'y' or FBMesDecision == 'Y':
+  print('Facebook Messenger data have been extracted from local files.')
+else:
+  print('No Facebook Messenger data will be collected, moving on!')
 
-
+#-----------------------------------------#
 ## TWITTER DATA
 
-# Query user for the Twitter user name, if not provided skip the rest of the Twitter code
-TwitName = input("Please enter your Twitter username and hit Enter (if you don't use Twitter, type N and hit Enter): ")
+# Ask user if they want to use Twitter data
+TwitDecision = input("Would you like to scrape data from your Twitter account? This will collect your Tweets. Y/N ")
 
 # Make sure that code is NOT run if user entered N
-if TwitName != 'N':
+if (TwitDecision == 'Y') or (TwitDecision == 'y'):
+
+  # Query user for the Twitter user name, if not provided skip the rest of the Twitter code
+  TwitName = input("Please enter your Twitter username and hit Enter: ")
 
   # Prepare variable
   tweet_collection = pd.DataFrame()
@@ -113,6 +135,9 @@ if TwitName != 'N':
   # Printing to text file (hopefully), each file's data to a new line
   with open("output_messages.txt", "a", encoding='utf-8') as f:
     print(list(tweet_collection.completion), file=f)
+
+  if FBMesDecision == "N" or FBMesDecision == "n":
+    userIdentifier = TwitName
 
   # Need to adapt this to have a matching amount of names (or maybe fix in R?)
   # Determine how many 'senders' there need to be fabricated
@@ -190,6 +215,8 @@ else:
     with open("output_senders.txt", "a") as f:
       print(list(senders), file=f) """
 
+#-----------------------------------------#
+
 ## FINAL TOUCH!
 
 # Having each data file on a new line is good for visual check, 
@@ -197,17 +224,22 @@ else:
 # which is what we'll do now for both the MESSAGES file and the SENDERS file
 # and save to new output files to be used in the next R script
 
-## For Messages file
-with open('output_messages.txt','r') as file1:
-    with open('output_messages_formatted.txt','w') as file2:
-        file2.write(file1.read().replace('\n', ''))
-## For Senders file
-with open('output_senders.txt','r') as file1:
-    with open('output_senders_formatted.txt','w') as file2:
-        file2.write(file1.read().replace('\n', ''))
+if (FBMesDecision == "Y") or (FBMesDecision == "y") or (TwitDecision == "Y") or (TwitDecision == "y"):
 
-# Print that everything is done.
-print('The scraping script is now done running. Smiley day to ya!')
+  ## For Messages file
+  with open('output_messages.txt','r') as file1:
+      with open('output_messages_formatted.txt','w') as file2:
+          file2.write(file1.read().replace('\n', ''))
+  ## For Senders file
+  with open('output_senders.txt','r') as file1:
+      with open('output_senders_formatted.txt','w') as file2:
+          file2.write(file1.read().replace('\n', ''))
+
+  # Print that everything is done.
+  print('The scraping script is now done running and data was collected. Smiley day to ya!')
+
+else:
+  print('The end of the scrqping script has been reached. No data were scraped. Smiley day to ya!')
 
 # IMPORTANT: AFTER YOU'VE DONE THIS, THERE IS ONE THING YOU NEED TO DO MANUALLY: 
 # GO TO THE VERY END OF THE LINE IN EACH FILE AND HIT ENTER (TO CREATE A NEW EMPTY LINE)
